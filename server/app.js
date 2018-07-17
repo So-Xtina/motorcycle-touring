49512a8ab1024bb5c28745327f1e458f716a9126
So-Xtina <mu7zic2soul@gmail.com>
2018-07-17 12:54:25 -0700

    completed setting up the server to use for the yelp endpoints

---
+++ server/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const yelp = require("yelp-fusion");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 9000;
+const API_KEY =
+	"ttxJcYPvjwpiLMw1Vz3OVGye2SK2vStLlW5dVwv3mF_SXEqCe-IO4nvPzBDXl60EkvJtygZmmmKhBVj3AE6JBU6c8rb_4TdLIYw3gMRBzgqchNOW_vtxJVuRmVNJW3Yx";
+const client = yelp.client(API_KEY);
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+//****************Endpoints start here****************/
+
+// app.get("/", (req, res) => {
+// 	// res.sendFile(__dirname + "/client/index.html");
+// });
+
+app.post("/yelp-search", async (req, res) => {
+	const { term, location } = req.body;
+	let output = {};
+
+	await client
+		.search({ term, location })
+		.then(response => {
+			output.success = true;
+			output.data = response.jsonBody.businesses;
+		})
+		.catch(err => {
+			output.success = false;
+			output.errors = JSON.parse(err.response.body);
+		});
+
+	res.json(output);
+});
+
+app.listen(PORT, () => {
+	console.log("Server started on PORT: ", PORT);
+});
