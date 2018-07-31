01e1c320a6d7371b69233ba3882a82087294a5d4
So-Xtina <mu7zic2soul@gmail.com>
2018-07-31 16:24:54 -0700

    changed the routes map design and when marker is clicked will open the routes tab

--- client/scripts.js
+++ client/scripts.js
@@ -300,14 +300,15 @@ function getBusiness() {
 //creating markers for the map and grabbing geolocation of user/directions for each yelp location
 var map, infoWindow, userLocation, popup, Popup;
 function initMap(data) {
-	debugger;
+	// debugger;
 	console.log("initMap data: ", data);
 
 	// var unitedStatesCenterPoint = { lat: 37.09024, lng: -95.712891 };
 	var map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 11,
 		center: userLocation,
-		mapTypeId: google.maps.MapTypeId.TERRAIN
+		// mapTypeId: google.maps.MapTypeId.TERRAIN
+		styles: mapStyle
 	});
 
 	// //Geocode, grabbing lat and lng
@@ -411,6 +412,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 //displaying the route
 function displayRoute(origin, destination, service, display) {
+	document.getElementById("routes").style.display = "block";
 	service.route(
 		{
 			origin: origin,
@@ -457,3 +459,332 @@ function loader() {
 	var page = document.querySelector("div.container");
 	page.appendChild(loader);
 }
+
+/******************* Map Style ********************/
+
+var mapStyle = [
+	{
+		featureType: "administrative",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#d6e2e6"
+			}
+		]
+	},
+	{
+		featureType: "administrative",
+		elementType: "geometry.stroke",
+		stylers: [
+			{
+				color: "#cfd4d5"
+			}
+		]
+	},
+	{
+		featureType: "administrative",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#7492a8"
+			}
+		]
+	},
+	{
+		featureType: "administrative.country",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				visibility: "simplified"
+			},
+			{
+				color: "#952f2f"
+			}
+		]
+	},
+	{
+		featureType: "administrative.neighborhood",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				lightness: 25
+			}
+		]
+	},
+	{
+		featureType: "landscape.man_made",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#e6e6e6"
+			},
+			{
+				saturation: "0"
+			}
+		]
+	},
+	{
+		featureType: "landscape.man_made",
+		elementType: "geometry.stroke",
+		stylers: [
+			{
+				color: "#d5d4cf"
+			}
+		]
+	},
+	{
+		featureType: "landscape.natural",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#7492a8"
+			}
+		]
+	},
+	{
+		featureType: "landscape.natural.terrain",
+		elementType: "all",
+		stylers: [
+			{
+				visibility: "off"
+			}
+		]
+	},
+	{
+		featureType: "poi",
+		elementType: "geometry",
+		stylers: [
+			{
+				visibility: "simplified"
+			}
+		]
+	},
+	{
+		featureType: "poi",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#e3e1dd"
+			},
+			{
+				visibility: "simplified"
+			}
+		]
+	},
+	{
+		featureType: "poi",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#6e6362"
+			},
+			{
+				weight: "2"
+			}
+		]
+	},
+	{
+		featureType: "poi",
+		elementType: "labels.icon",
+		stylers: [
+			{
+				saturation: -100
+			},
+			{
+				visibility: "simplified"
+			}
+		]
+	},
+	{
+		featureType: "poi.business",
+		elementType: "all",
+		stylers: [
+			{
+				visibility: "off"
+			}
+		]
+	},
+	{
+		featureType: "poi.park",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#a9de83"
+			}
+		]
+	},
+	{
+		featureType: "poi.park",
+		elementType: "geometry.stroke",
+		stylers: [
+			{
+				color: "#bae6a1"
+			}
+		]
+	},
+	{
+		featureType: "poi.sports_complex",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#c6e8b3"
+			}
+		]
+	},
+	{
+		featureType: "poi.sports_complex",
+		elementType: "geometry.stroke",
+		stylers: [
+			{
+				color: "#bae6a1"
+			}
+		]
+	},
+	{
+		featureType: "road",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#41626b"
+			}
+		]
+	},
+	{
+		featureType: "road",
+		elementType: "labels.icon",
+		stylers: [
+			{
+				saturation: -45
+			},
+			{
+				lightness: 10
+			},
+			{
+				visibility: "on"
+			}
+		]
+	},
+	{
+		featureType: "road.highway",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#c1d1d6"
+			}
+		]
+	},
+	{
+		featureType: "road.highway",
+		elementType: "geometry.stroke",
+		stylers: [
+			{
+				color: "#a6b5bb"
+			}
+		]
+	},
+	{
+		featureType: "road.highway",
+		elementType: "labels.icon",
+		stylers: [
+			{
+				visibility: "on"
+			}
+		]
+	},
+	{
+		featureType: "road.highway.controlled_access",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#9fb6bd"
+			}
+		]
+	},
+	{
+		featureType: "road.arterial",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#ffffff"
+			}
+		]
+	},
+	{
+		featureType: "road.local",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#ffffff"
+			}
+		]
+	},
+	{
+		featureType: "transit",
+		elementType: "labels.icon",
+		stylers: [
+			{
+				saturation: -70
+			}
+		]
+	},
+	{
+		featureType: "transit.line",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#b4cbd4"
+			}
+		]
+	},
+	{
+		featureType: "transit.line",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#588ca4"
+			}
+		]
+	},
+	{
+		featureType: "transit.station",
+		elementType: "all",
+		stylers: [
+			{
+				visibility: "off"
+			}
+		]
+	},
+	{
+		featureType: "transit.station",
+		elementType: "labels.text.fill",
+		stylers: [
+			{
+				color: "#008cb5"
+			},
+			{
+				visibility: "on"
+			}
+		]
+	},
+	{
+		featureType: "transit.station.airport",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				saturation: -100
+			},
+			{
+				lightness: -5
+			}
+		]
+	},
+	{
+		featureType: "water",
+		elementType: "geometry.fill",
+		stylers: [
+			{
+				color: "#a6cbe3"
+			}
+		]
+	}
+];
